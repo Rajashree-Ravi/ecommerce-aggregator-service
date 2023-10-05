cad97b79b8d8008e9ea43f91a539aaa6e98643e5
Rajashree Ravi <rajashreeravi09@gmail.com>
2023-10-05 06:56:11 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/aggregatorservice/EcommerceAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.aggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAggregatorServiceApplication.class, args);
+	}
+
+}
